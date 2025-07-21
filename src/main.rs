7ce09d6cef7320a5e529cba221a2d0623aaebbe2
pengjj <121699842@qq.com>
2025-07-21 13:30:07 +0800

    feat:修改一些依赖值

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,13 @@ mod io;
 mod method;
 mod rscode;
 
-use bytes::BytesMut;
-use log::{info, log};
+use log::{info};
 use rmp_serde::to_vec_named;
-use std::collections::HashMap;
-use std::io::{Error, ErrorKind, Read};
-use std::panic;
 use std::sync::Arc;
-use tokio::io::{AsyncReadExt, BufWriter};
-use tokio::net::{TcpListener, TcpStream};
+use tokio::net::{TcpListener};
 use tokio::sync::Mutex;
-
+use tokio::io::AsyncWriteExt;
+use db::CacheDb;
 #[tokio::main]
 async fn main() {
     log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
@@ -54,60 +50,4 @@ async fn main() {
             }
         });
     }
-}
-
-use crate::db::CacheDb;
-use tokio::io::AsyncWriteExt;
-
-#[tokio::main]
-async fn main1() -> Result<(), Box<dyn std::error::Error>> {
-    let listener = TcpListener::bind("127.0.0.1:8080").await?;
-
-    loop {
-        let (mut socket, _) = listener.accept().await?;
-
-        tokio::spawn(async move {
-            // In a loop, read data from the socket and write the data back.
-            loop {
-                match process(&mut socket).await {
-                    Ok(_) => {}
-                    Err(e) => {
-                        println!("{:#?}", e);
-                        break;
-                    }
-                };
-            }
-        });
-    }
-}
-
-async fn process(socket: &mut TcpStream) -> Result<(), Error> {
-    let mut buff = vec![];
-    let mut steam = BufWriter::new(socket);
-    loop {
-        let mut buf = BytesMut::with_capacity(1);
-        // steam.read_buf(&mut buf).await?;
-        let n = match steam.read_buf(&mut buf).await {
-            // socket closed
-            Ok(n) if n == 0 => {
-                println!("read 0 bytes");
-                return Err(Error::new(ErrorKind::Other, "read 0 bytes"));
-            }
-            Ok(n) => n,
-            Err(e) => {
-                eprintln!("failed to read from socket; err = {:?}", e);
-                return Err(Error::new(
-                    ErrorKind::Other,
-                    "failed to read from socket; err = {:?}",
-                ));
-            }
-        };
-        println!("read {} bytes", n);
-        buff.extend_from_slice(&buf[..n]);
-    }
-    if buff.len() == 0 {
-        println!("eof");
-    }
-    // Write the data back
-    socket.write_all(&buff).await
 }
